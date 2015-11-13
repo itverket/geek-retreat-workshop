fc23c66e1b89a1bb4f52542b3445273aff8faa13
mellef89 <m.ellef@gmail.com>
2015-11-13 19:21:53 +0100

    added files for premade front end

---
+++ Documents/AzureSearchResult.cs
@@ -0,0 +1,13 @@
+﻿using Entities.Twitter.SearchIndex;
+using Microsoft.Azure.Search.Models;
+using System.Collections.Generic;
+
+namespace Web.ViewModels
+{
+    public class AzureSearchResult
+    {
+        public long? Count { get; set; }
+        public FacetResults Facets { get; set; }
+        public IList<SearchResult<FlattendTweet>> Tweets { get; set; }
+    }
+}
